a66734540739b6f438c91a3c1cf97303cdcfb957
BigPeter <1358321209@qq.com>
2019-04-10 14:35:21 +0800

    add intm 9 code example

--- src/com/wj/example/item9/TryWithResourcesDemo.java
+++ src/com/wj/example/item9/TryWithResourcesDemo.java
@@ -2,7 +2,34 @@ package com.wj.example.item9;
 
 import java.io.*;
 
+/*
+    Refer try-with-resources to try-finally
+ */
+
+
 public class TryWithResourcesDemo {
+
+    static String testFinallyException(String filePath) throws IOException {
+
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader(filePath));
+        } finally {
+            if (br != null){
+                br.close();
+            }
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    static String testException(String filePath) throws IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath));){
+            if (filePath.equals(""))
+                throw new IllegalArgumentException();
+            return br.readLine();
+        }
+    }
 
     // try-with-resources with a catch clause
     static String firstLineOfFile(String filePath, String defaultValue){
@@ -14,7 +41,27 @@ public class TryWithResourcesDemo {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            testFinallyException("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Thread.sleep(1000); // to make console output format
+
+        System.out.println("-------------------------------------\n");
+
+        try {
+            testException("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Thread.sleep(1000);
+
+        System.out.println("-------------------------------------\n");
+
         String line = firstLineOfFile("", "Hello World!");
         System.out.println(line);
     }
